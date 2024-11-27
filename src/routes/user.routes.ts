313c14e0626e9ab92d86f1b251d840fd2ec92a3b
Matheus Henrique Almeida Vieira Neves <matheus16092004@gmail.com>
2024-11-26 23:17:00 -0300

    mudano post

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,7 +7,7 @@ import { auth } from '../middlewares/auth.middleware'
 const router = Router() // Cria uma instância do Router
 //Todas as rotas foram verificadas e estão funcionando corretamente
 //Corigidas por: Matheus Henrique
-router.post('/',auth,validate(CreateUserDto), createUser) // Define a rota para criar um usuário
+router.post('/',validate(CreateUserDto), createUser) // Define a rota para criar um usuário
 router.get('/',auth ,findAllUsers) // Define a rota para buscar todos os usuários
 router.patch('/:id',auth , validate(UpdateUserDto), updateUser) // Define a rota para atualizar um usuário
 router.delete('/:id',auth , deleteUser) // Define a rota para deletar um usuário
